3899766bffd0a775dfb3b3aa3322ba0545bceff5
Anonymous123xx <97088225+Anonymous123xx@users.noreply.github.com>
2022-10-13 23:00:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import os
-
 from utils2 import *
 
 class Config_RL_multistep:
@@ -28,9 +26,8 @@ class Config_RL_multistep:
 
 if __name__ == "__main__":
     model_name = 'multistep_RLRep'
-    path = sys.argv[1]  # dataset_vul/newALLBUGS
-    os.makedirs('{}/log'.format(path), exist_ok=True)
-    logger = get_logger('{}/log/{}_logging.txt'.format(path, model_name))
+    path = 'dataset_vul/newALLBUGS'
+    logger = get_logger('dataset_vul/newALLBUGS/log/{}_logging.txt'.format(model_name))
     with open('{}/code_w2i.pkl'.format(path), 'rb') as f, open('{}/ast_w2i.pkl'.format(path), 'rb') as f2:
         code_w2i = pickle.load(f)
         ast_w2i = pickle.load(f2)
@@ -47,27 +44,26 @@ if __name__ == "__main__":
         in_w2i = (code_w2i, ast_w2i)
         model = Model(config)
         beam_search_use = 5
-        if not start == -1:
-            model_path = ''
-            model.load(model_path)
+        os.makedirs('dataset_vul/newALLBUGS/model', exist_ok=True)
+        if start != -1:
+            model.load('dataset_vul/newALLBUGS/model/multistep_RLRep_33')
             model.set_trainer()
 
-        os.makedirs('{}/model'.format(path), exist_ok=True)
         for epoch in range(start+1, config.PRE_EPOCH):
             loss = 0
-            code_dir = '{}/pretrain/threelines-tokenseq'.format(path)
-            ast_dir = '{}/pretrain/ast'.format(path)
+            code_dir = 'dataset_vul/newALLBUGS/pretrain/threelines-tokenseq'
+            ast_dir = 'dataset_vul/newALLBUGS/pretrain/ast'
             for step, batch in enumerate(get_batch(code_dir, ast_dir, config, in_w2i, pretrain=True)):
                 batch_in1, batch_in2, batch_in3, batch_out = batch
                 loss += model.pretrain(batch[:-2], batch[-1], 'actor')
                 logger.info('Epoch: {}, Batch: {}, Loss: {}'.format(epoch, step, loss / (step + 1)))
-            model.save('{}/model/{}_{}'.format(path, model_name, epoch))
+            model.save('dataset_vul/newALLBUGS/model/{}_{}'.format(model_name, epoch))
 
         for epoch in range(config.PRE_EPOCH, config.EPOCH):
             loss_actor = 0
             loss_critic = 0.
-            code_dir = '{}/threelines-tokenseq'.format(path)
-            ast_dir = '{}/ast'.format(path)
+            code_dir = 'dataset_vul/newALLBUGS/threelines-tokenseq'
+            ast_dir = 'dataset_vul/newALLBUGS/ast'
             for step, batch in enumerate(get_batch(code_dir, ast_dir, config, in_w2i, pretrain=False)):
                 batch_in1, batch_in2, batch_in3 = batch
                 loss = model(batch[:-1], True, batch[-1])
@@ -75,8 +71,8 @@ if __name__ == "__main__":
                 logger.info('Epoch: {}, Batch: {}, Loss: actor:{}'.format(epoch, step, loss_actor / (step + 1),))
 
             preds, ats, names, rewards = [], [], [], []
-            valid_code_dir = "{}/validation/threelines-tokenseq".format(path)
-            valid_ast_dir = "{}/validation/ast".format(path)
+            valid_code_dir = "dataset_vul/newALLBUGS/validation/threelines-tokenseq"
+            valid_ast_dir = "dataset_vul/newALLBUGS/validation/ast"
             for step, batch in enumerate(get_batch(valid_code_dir, valid_ast_dir, config, in_w2i, pretrain=False)):
                 batch_in1, batch_in2, batch_in3 = batch
                 if beam_search_use > 0:
@@ -90,15 +86,6 @@ if __name__ == "__main__":
             for tup in zip(ats, names):
                 reward = choose_action(tup[1], tup[0], False)
                 rewards.append(reward)
-                os.makedirs('dataset_vul/newALLBUGS/validation/result/', exist_ok=True)
-                os.makedirs("dataset_vul/newALLBUGS/validation/result/{}/original_contract".format(epoch), exist_ok=True)
-                os.makedirs("dataset_vul/newALLBUGS/validation/result/{}/repair_contract".format(epoch), exist_ok=True)
-                src_path = "dataset_vul/newALLBUGS/validation/contract/{}.sol".format(name)
-                copy_paths = "dataset_vul/newALLBUGS/validation/result/{0}/original_contract/{1}.sol".format(epoch, name)
-                repair_paths = "dataset_vul/newALLBUGS/repair_contract/{}.sol".format(name)
-                copy_paths2 = "dataset_vul/newALLBUGS/validation/result/{0}/repair_contract/{1}.sol".format(epoch, name)
-                shutil.copy(src_path, copy_paths)
-                shutil.copy(repair_paths, copy_paths2)
 
             positive_repair = 0
             for x in rewards:
@@ -107,7 +94,7 @@ if __name__ == "__main__":
             if positive_repair > best_positive_repair:
                 print('model update. the {0}-th epoch. positive reward / total : {1} / {2}'.format(epoch, positive_repair, len(names)))
                 best_positive_repair = positive_repair
-                model.save('{}/model/{}_{}'.format(path, model_name, epoch))
+                model.save('dataset_vul/newALLBUGS/model/{}_{}'.format(model_name, epoch))
             else:
                 print('model NOT update. the {0}-th epoch. positive reward / total : {1} / {2}'.format(epoch, positive_repair, len(names)))
 
